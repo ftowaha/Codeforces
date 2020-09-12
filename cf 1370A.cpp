a5882cdd89cbe94b1894cecb55d7d338a7cafc76
Faysal Rahman <64473571+ftowaha@users.noreply.github.com>
2020-09-13 01:37:31 +0600

    Add files via upload

---
+++ cf 1370A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i,a=0,j,b,c;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&b);
+        printf("%d\n",b/2);
+    }
+    return 0;
+}
